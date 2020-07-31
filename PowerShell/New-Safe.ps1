f447090a02176299d74fff78a6677cb1ce56e4da
adamchandler0306 <adam@pop-os.localdomain>
2020-07-31 10:06:24 -0500

    Updated Credential handling

--- PowerShell/New-Safe.ps1
+++ PowerShell/New-Safe.ps1
@@ -4,19 +4,16 @@ Install-Module CredentialRetriever
 # Retrieving the Credential to be used to create the new safe via Central Credential Provider.
 $result = Get-CCPCredential -AppID App -Safe Safe -Object ObjectName -URL https://pvwa
 $result.ToCredential()
-$user = $result.ToCredential().username
-$pass = $result.ToCredential().content
+$credentials = $result.ToCredential()
 # Creating session to CyberArk to create safe with credentials retrieved from Central Credential Provider.
-New-PASSession -Credential $result -BaseURI https://pvwa
+New-PASSession -Credential $credentials -BaseURI https://pvwa
 # Creating a new safe.
 Add-PASSafe -SafeName  -Description C7 -ManagingCPM PasswordManager -NumberOfDaysRetention 7
 # Adding the Vault Admins group to the safe members with all permissions.
 Add-PASSafeMember -SafeName  -MemberName "Vault Admins" -SearchIn Vault -UseAccounts $true -RetrieveAccounts $true -ListAccounts $true -AddAccounts $true -UpdateAccountContent $true -UpdateAccountProperties $true -InitiateCPMAccountManagementOperations $true -SpecifyNextAccountContent $true -RenameAccounts $true -DeleteAccounts $true -UnlockAccounts $true -ManageSafe $true -ManageSafeMembers $true -BackupSafe $true -ViewAuditLog $true -ViewSafeMembers $true -AccessWithoutConfirmation $true -CreateFolders $true -DeleteFolders $true -MoveAccountsAndFolders $true   
-# Adding Second Vault Admins group to the safe members with all permissions.
-Add-PASSafeMember -SafeName  -MemberName "Safe Admins" -SearchIn domain -UseAccounts $true -RetrieveAccounts $true -ListAccounts $true -AddAccounts $true -UpdateAccountContent $true -UpdateAccountProperties $true -InitiateCPMAccountManagementOperations $true -SpecifyNextAccountContent $true -RenameAccounts $true -DeleteAccounts $true -UnlockAccounts $true -ViewAuditLog $true -ViewSafeMembers $true -AccessWithoutConfirmation $true -MoveAccountsAndFolders $true          
 # Adding the safe user LDAP based group to the safe members.
 # TODO: Define new user permissions.
 Add-PASSafeMember -SafeName  -MemberName "" -SearchIn domain -UseAccounts $true -RetrieveAccounts $true -ListAccounts $true -AddAccounts $true -UpdateAccountContent $true -UpdateAccountProperties $true -InitiateCPMAccountManagementOperations $true -SpecifyNextAccountContent $true -RenameAccounts $true -DeleteAccounts $true -UnlockAccounts $true -ViewAuditLog $true -ViewSafeMembers $true -AccessWithoutConfirmation $true -MoveAccountsAndFolders $true       
 # Removing the User from the safe members that created the safe.
-Remove-PASSafeMember -SafeName  -MemberName    
+Remove-PASSafeMember -SafeName  -MemberName "retrieved user"
 
